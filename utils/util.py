e56aa98f7ba5679cc88bb698c4eba8eb28b14262
dlm <136665323@qq.com>
2018-01-22 23:57:34 +0800

    utilstils help

---
+++ utils/util.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding -*-
+
+
+def add(x, y):
+    """计算 两个数的和
+    :param x: int
+    :param y: int
+    """
+    return x + y
+
+
+def multiplication(x, y):
+    """计算 两个数的乘
+    :param x: int
+    :param y: int
+    """
+    return x * y
---
+++ yanghuisanjiao.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding -*-
+
+
+def triangle():
+    # TODO: 下次完成
+    pass
+
+
+if __name__ == '__main__':
+    triangle()
